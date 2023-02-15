36b95351b96f7c7c56099bac1d155cacb3339992
Supremo-N9 <125316216+Supremo-N9@users.noreply.github.com>
2023-02-14 23:12:15 -0600

    Update recibir.py

--- recibir.py
+++ recibir.py
@@ -1,12 +1,6 @@
 import socket
 
-server_ip = '192.168.1.68'
-server_port = 5555
-
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((server_ip, server_port))
-
-print("Conexión establecida")
+client_socket.connect(('192.168.1.68', 5555))
 print(client_socket.recv(1024).decode())
-
 client_socket.close()
