3b45028c89a4bc42a3c9a6ac5c24d4f87b8300ac
Supremo-N9 <125316216+Supremo-N9@users.noreply.github.com>
2023-02-14 23:15:07 -0600

    Update recibir.py

--- recibir.py
+++ recibir.py
@@ -1,6 +1,15 @@
 import socket
 
+server_address = ('192.168.1.68', 5555)
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('192.168.1.68', 5555))
-print(client_socket.recv(1024).decode())
+client_socket.connect(server_address)
+print("Conexión establecida")
+
+while True:
+    message = input("Mensaje: ")
+    client_socket.send(message.encode())
+    response = client_socket.recv(1024).decode()
+    print(f"Recibido: {response}")
+    
 client_socket.close()
